891c5f68aac018c08d34c8c0e4924de8bebe6062
alexisjaymes <emovonjames@gmail.com>
2022-09-08 22:51:03 +0100

    Initial Commit

---
+++ JayCryptoApp/JayCryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JayCryptoAppApp.swift
+//  JayCryptoApp
+//
+//  Created by Alexis Jaymes on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct JayCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
